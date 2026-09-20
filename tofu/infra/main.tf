33fb87ee9bc3af29512c610a453c188908d4b71d
Andrei Margaev <andrei.margaev@gmail.com>
2026-09-21 00:21:44 +0200

    unite

--- tofu/infra/main.tf
+++ tofu/infra/main.tf
@@ -51,3 +51,34 @@ resource "helm_release" "cert-manager" {
     file("${path.module}/cert-manager-values.yaml")
   ]
 }
+
+resource "kubernetes_manifest" "argocd_project" {
+  manifest = yamldecode(templatefile("${path.module}/templates/argocd-project.yaml.tftpl", {
+    argocd_namespace = var.argocd_namespace
+  }))
+
+  depends_on = [
+    helm_release.argocd,
+  ]
+}
+
+resource "kubernetes_manifest" "argocd_application_set" {
+  manifest = yamldecode(templatefile("${path.module}/templates/argocd-application-set.yaml.tftpl", {
+    argocd_namespace = var.argocd_namespace
+  }))
+
+  depends_on = [
+    kubernetes_manifest.argocd_project,
+  ]
+}
+
+resource "kubernetes_manifest" "argocd_image_updater" {
+  manifest = yamldecode(templatefile("${path.module}/templates/argocd-image-updater.yaml.tftpl", {
+    argocd_namespace = var.argocd_namespace
+  }))
+
+  depends_on = [
+    kubernetes_manifest.argocd_application_set,
+  ]
+}
+
